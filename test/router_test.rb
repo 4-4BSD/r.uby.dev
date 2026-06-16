e21ef697700d70e2f48b853611395cdb1b60b4d5
robert <0x1eef@hardenedbsd.org>
2026-06-16 07:19:17 +0000

    add tests

---
+++ test/router_test.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require_relative "setup"
+
+class RouterTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def test_guides_index
+    get "/api/guides/index"
+    assert_equal last_response.status, 200
+    assert_equal body["ok"], true
+  end
+
+  def test_guides_search
+    get "/api/guides/search", q: "Getting Started"
+    assert_equal last_response.status, 200
+    assert_equal body["ok"], true
+    assert_includes body["stdout"], "getting-started.md"
+  end
+
+  def test_guides_read
+    get "/api/guides/read", q: "getting-started"
+    assert_equal last_response.status, 200
+    assert_equal body["ok"], true
+    assert_includes body["contents"], "Getting Started with mruby"
+  end
+
+  def test_mruby_index
+    get "/api/mruby/index"
+    assert_equal last_response.status, 200
+    assert_equal body["ok"], true
+  end
+
+  def test_mruby_search
+    get "/api/mruby/search", q: "mrb_obj_eq"
+    assert_equal last_response.status, 200
+    assert_equal body["ok"], true
+    assert_includes body["stdout"], "object.c"
+  end
+
+  def test_mruby_read
+    get "/api/mruby/read", q: "object.c"
+    assert_equal last_response.status, 200
+    assert_equal body["ok"], true
+    assert_includes body["contents"], "mrb_obj_eq"
+  end
+
+  def test_mrbgem_index
+    get "/api/mrbgems/index"
+    assert_equal last_response.status, 200
+    assert_equal body["ok"], true
+  end
+
+  def test_mrbgem_search
+    get "/api/mrbgems/search", q: "json"
+    assert_equal last_response.status, 200
+    assert_equal body["ok"], true
+    assert_includes body["stdout"], "mruby-json.gem"
+  end
+
+  def test_mrbgem_read
+    get "/api/mrbgems/read", q: "mruby-json"
+    assert_equal last_response.status, 200
+    assert_equal body["ok"], true
+    assert_includes body["contents"], "JavaScript Object Notation"
+  end
+
+  private
+
+  def app
+    Router
+  end
+
+  def body
+    JSON.parse(last_response.body)
+  end
+end
---
+++ test/setup.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require "rack/test"
+require File.join(__dir__, "..", "lib", "init")
